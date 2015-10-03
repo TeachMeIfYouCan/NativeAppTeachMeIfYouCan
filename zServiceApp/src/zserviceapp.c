271a88a106f2cfbf43fece46018806c7e4f2cc93
chudaeyoum <chudaeyoun@naver.com>
2015-10-04 03:16:57 +0900

    공개 SW 제출 코드

--- zServiceApp/src/zserviceapp.c
+++ zServiceApp/src/zserviceapp.c
@@ -23,13 +23,14 @@ void audio_send_byte(char* char_buffer);
 
 //char* remote_app_id_copy = "S8vjRcPYft.zWebApp";
 
-char* remote_app_id_copy= "Y2QbwJHU6E.TizenProjectTeachMeIFYouCanVER01";
+char* remote_app_id_copy= "jeITKUm4Mv.TeachMeIFYouCan";
 
 char encoding[256][2];
 
 pthread_t p_thread;
 int audio_signal_check = 0;
 int audio_exit_check = 0;
+int audio_in_start_check = 0;
 
 audio_in_h input; // Declare audio input handle
 audio_out_h output; // Declare audio output handle
@@ -112,13 +113,11 @@ void b64_encode(char *clrstr, char *b64dst) {
 }
 
 int audio_init() {
-
 	//ret = audio_in_create(SAMPLE_RATE, AUDIO_CHANNEL_MONO, AUDIO_SAMPLE_TYPE_S16_LE, &input);
 	//ret = audio_out_create(SAMPLE_RATE, AUDIO_CHANNEL_MONO, AUDIO_SAMPLE_TYPE_S16_LE, SOUND_TYPE_SYSTEM, &output);
 
 	ret = audio_in_create(SAMPLE_RATE, AUDIO_CHANNEL_STEREO, AUDIO_SAMPLE_TYPE_U8, &input);
 	ret = audio_out_create(SAMPLE_RATE, AUDIO_CHANNEL_STEREO, AUDIO_SAMPLE_TYPE_U8, SOUND_TYPE_SYSTEM, &output);
-
 
 	//ret = audio_in_get_buffer_size(input, &buffer_size);
 
@@ -136,44 +135,9 @@ int audio_init() {
 	return 1;
 }
 
-void convert_init() {
-	int k = 0;
-
-	for (int i = 'a'; i <= 'z'; i++){
-
-		for (int j = 'a'; j<='z'; j++){
-
-			encoding[k][0] = i;
-			encoding[k][1] = j;
-
-			k++;
-			if (k == 256)
-				break;
-		}
-		if (k == 256)
-			break;
-	}
-}
-
-char* convert(int ret){
-	if(ret >= 256)
-		dlog_print(DLOG_INFO, "chu", "convert 256 over");
-
-	return encoding[ret];
-}
-
-char deconvert(char* ret){
-	for(int i = 0; i <256; i++)
-		if(ret[0] == encoding[i][0] && ret[1] == encoding[i][1])
-			return i;
-
-	dlog_print(DLOG_INFO, "chu", "deconvert Abnormal");
-	return '@';
-}
-
 void* audio_in_start() {
 
-	int k = 0;
+	audio_in_start_check = 1;
 
 	while(1) {
 		while(audio_signal_check == 0) {
@@ -183,55 +147,17 @@ void* audio_in_start() {
 			ret = audio_in_unprepare(input);
 
 			char* temp_buffer = buffer;
-
-			//char* send_buffer = (char*)malloc(2 * buffer_size);
-			//memset(send_buffer, 0, 2 * buffer_size);
-			//memset(send_buffer, 0, 2 * buffer_size);
-
-			//char* send_buffer;
 			char send_buffer[512];
-			//memset(send_buffer, 0, buffer_size);
+			memset(send_buffer, 0, buffer_size);
 
-			//size_t olen = buffer_size;
-			//size_t buSize = buffer_size;
-
-			dlog_print(DLOG_INFO, "chu_audio_in", "==================");
-			dlog_print(DLOG_INFO, "chu_audio_in", "==================");
-
+			//dlog_print(DLOG_INFO, "chu_audio_in", "==================");
 			b64_encode(temp_buffer, send_buffer);
-			/*
-
-			if(mbedtls_base64_encode( temp_buffer, buSize, &olen, buffer, buffer_size ) == 0)
-					dlog_print(DLOG_INFO, "chu_audio_in", "tmbedtls_base64_encode success");
-			*/
-
-			/*
-			for(int i = 0; i < 10; i++) {
-				dlog_print(DLOG_INFO, "chu_audio_in", "atfer send_buffer[%d] = %c", i, send_buffer[i]);
-			}
-			 */
-
-
-			/*
-			for(int i = 0; i < buffer_size; i++) {
-				char* temp = convert(temp_buffer[i]);
-				send_buffer[k++]  = temp[0];
-				send_buffer[k++]  = temp[1];
-			}
-
-			//dlog_print(DLOG_INFO, "chu_audio_in", "=====================================================================");
-		    //dlog_print(DLOG_INFO, "chu_audio_in", "temp_buffer len = %d val = %s", strlen(temp_buffer), temp_buffer);
 			//dlog_print(DLOG_INFO, "chu_audio_in", "send_buffer len = %d val = %s", strlen(send_buffer), send_buffer);
-			*/
 			audio_send_byte(send_buffer);
-
-			//free(send_buffer);
-			k = 0;
 
 			if(audio_exit_check == 1)
 				break;
 		}
-
 		if(audio_exit_check == 1) {
 			audio_exit_check = 0;
 			break;
@@ -244,22 +170,16 @@ void* audio_in_start() {
 void audio_send_byte(char* char_buffer){
 
 	bundle *b = bundle_create();
-
 	bundle_add_str(b, "foo", char_buffer);
-
 	message_port_send_message(remote_app_id_copy, "RECEIVE_HELLO_MESSAGE", b);
-
 	bundle_free(b);
 }
 
 void message_port_cb(int local_port_id, const char *remote_app_id,
-		const char *remote_port, bool trusted_remote_port, bundle *message) {
+		const char *remote_port, bool trusted_remote_port, bundle *message, void* n) {
 
 	char *name = NULL;
 	bundle_get_str(message, "command", &name); //Web에서 "name"이라는 키를 받고  name변수에 값인 "Web"을 저장
-
-	//To print received message
-	//dlog_print(DLOG_INFO, "chu message receive", "Message from %s, name: %s ", remote_app_id, name);
 
 	if(strcmp(name, "audio_start") == 0) {
 		dlog_print(DLOG_INFO, "chu message receive", "command audio_start");
@@ -270,19 +190,24 @@ void message_port_cb(int local_port_id, const char *remote_app_id,
 		}
 		audio_signal_check = 0;
 		//dlog_print(DLOG_INFO, "chu message receive", "command audio_signal_check = %d audio_exit_check = %d", audio_signal_check, audio_exit_check );
-	}
-	else if(strcmp(name, "audio_pause") == 0) {
+	} else if(strcmp(name, "audio_pause") == 0) {
 		dlog_print(DLOG_INFO, "chu message receive", "command audio_pause");
 		audio_signal_check = 1;
 		//dlog_print(DLOG_INFO, "chu message receive", "command audio_signal_check = %d audio_exit_check = %d", audio_signal_check, audio_exit_check );
-	}
-	else if(strcmp(name, "audio_stop") == 0) {
+	} else if(strcmp(name, "audio_stop") == 0) {
 		dlog_print(DLOG_INFO, "chu message receive", "command audio_stop");
-		audio_signal_check = 1;
+		if(audio_in_start_check == 1)
+			audio_signal_check = 1;
 		//audio_signal_check = 0;
 		//audio_exit_check = 1;
 		//dlog_print(DLOG_INFO, "chu message receive", "command audio_signal_check = %d audio_exit_check = %d", audio_signal_check, audio_exit_check );
+	} else if(strcmp(name, "app_exit") == 0) {
+		dlog_print(DLOG_INFO, "chu message receive", "command app_exit");
+		audio_signal_check = 1;
+		audio_exit_check = 1;
+		service_app_exit();
 	}
+
 }
 
 void audio_message_port_cb(int local_port_id, const char *remote_app_id,
@@ -296,28 +221,10 @@ void audio_message_port_cb(int local_port_id, const char *remote_app_id,
 
 	b64_decode(recv_buffer, res_buffer);
 
-	/*
-	if(strlen(recv_buffer) > 1) {
-		for(int i = 0; i < buffer_size; i++) {
-			char temp[2];
-			char _temp;
-			temp[0] = recv_buffer[i * 2];
-			temp[1] = recv_buffer[i * 2 + 1];
-
-			_temp = deconvert(temp);
-
-			res_buffer[i] = _temp;
-		}
-	}
-	*/
-	//dlog_print(DLOG_INFO, "chu_audio_out", "recv_buffer len = %d val = %s", strlen(recv_buffer), recv_buffer);
-	dlog_print(DLOG_INFO, "chu_audio_out", "res_buffer  len = %d val = %s", strlen(res_buffer), res_buffer);
-
+	//dlog_print(DLOG_INFO, "chu_audio_out", "res_buffer  len = %d val = %s", strlen(res_buffer), res_buffer);
 	ret = audio_out_prepare(output);
 	ret = audio_out_write(output, (void*)res_buffer, buffer_size);
 	ret = audio_out_unprepare(output);
-
-	//free(res_buffer);
 }
 
 
@@ -325,8 +232,7 @@ bool service_app_create(void *data)
 {
 	dlog_print(DLOG_INFO, "NativeService", "Created.");
 
-	audio_init();
-	convert_init();
+	audio_init(); //오디오 초기화
 
 	int local_port_id = message_port_register_local_port("COMMAND_VALUE", message_port_cb, NULL);
 	int local_port_id2 = message_port_register_local_port("RECEIVE_AUDIO", audio_message_port_cb, NULL);
